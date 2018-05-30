30e68c8efe4e9dca8922266951f9ad7356798bb2
Rick Terrill <rick@bigswing.com>
2018-05-30 14:44:54 -0500

    fixed a bug where the overview text would just disappear instead of being concealed by the animation

--- AxioFlix/AxioFlix/MovieTableViewCell.swift
+++ AxioFlix/AxioFlix/MovieTableViewCell.swift
@@ -17,22 +17,35 @@ class MovieTableViewCell: UITableViewCell {
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var overviewContainer: UIView! {
         didSet {
+            // start out with the overview hidden
             overviewContainer.isHidden = true
         }
     }
     
+    // a convenient way to talk to this cell's TVC
     weak var tableViewController: MoviesTableViewController?
     var movieId:Int64?
     
     @IBAction func showHideTouchUp(_ sender: Any) {
-        let willBeHidden = !self.overviewContainer.isHidden
-        if let mId = self.movieId {
-//            self.tableViewController?.setMovie(movieId: mId, isOpen: !willBeHidden)
+        UIView.animate(withDuration: 0.3) {
+            
+            // toggle between hidden and shown
+            let willBeHidden = !self.overviewContainer.isHidden
+            
+            // update TVC state regarding show/hide status of this movie
+            if let mId = self.movieId {
+                self.tableViewController?.setMovie(movieId: mId, isOpen: !willBeHidden)
+            }
+            
+            // swap the title of the Show/Hide button
+            self.showHideButton.setTitle(willBeHidden ? "Show Overview" : "Hide Overview", for: .normal)
+            
+            // toggling isHidden will trigger UIStackView to animate
+            self.overviewContainer.isHidden = willBeHidden
+            self.layoutIfNeeded()
         }
-//        self.showHideButton.setTitle(willBeHidden ? "Show Overview" : "Hide Overview", for: .normal)
-        self.overviewContainer.isHidden = willBeHidden
-//        self.layoutIfNeeded()
         
+        // tell the TableView to animate
         if let table = self.tableViewController?.tableView {
             table.beginUpdates()
             table.endUpdates()
@@ -47,6 +60,6 @@ class MovieTableViewCell: UITableViewCell {
     
     // default to the closed state
     override func prepareForReuse() {
-//        self.overviewContainer.isHidden = true
+        self.overviewContainer.isHidden = true
     }
 }
