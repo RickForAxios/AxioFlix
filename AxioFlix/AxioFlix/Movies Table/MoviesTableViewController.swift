b479d094711ef3afa970fbea568e52502110d8bb
Rick Terrill <rick@bigswing.com>
2018-05-30 20:44:42 -0500

    cleanup and scar tissue removal

--- AxioFlix/AxioFlix/Movies Table/MoviesTableViewController.swift
+++ AxioFlix/AxioFlix/Movies Table/MoviesTableViewController.swift
@@ -29,12 +29,6 @@ class MoviesTableViewController: UITableViewController, NSFetchedResultsControll
         Api.sharedInstance.fetchMovies(page: TmdbMoviesPage)
 
         self.tableView.allowsSelection = false
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,16 +51,4 @@ class MoviesTableViewController: UITableViewController, NSFetchedResultsControll
             fatalError("Failed to initialize FetchedResultsController: \(error)")
         }
     }
-
-    // MARK: - Table view data source
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
